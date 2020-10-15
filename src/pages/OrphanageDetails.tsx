1a59b4ae831e236d3b800e5610daf3d77703ac41
jonashenrique90 <jonas.herique90gmail.com>
2020-10-15 11:27:37 -0300

    Adicionando mobile ao projeto

---
+++ src/pages/OrphanageDetails.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+
+const OrphanageDetails = () => {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.textDetails}> Olá Orfãos, bom dia</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	textDetails: {
+		color: '#000',
+	},
+});
+
+export default OrphanageDetails;
